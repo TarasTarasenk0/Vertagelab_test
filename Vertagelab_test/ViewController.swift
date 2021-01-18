182f1d656bcb3cdb8c00d7ed84be6fd2c359c0dc
Taras Tarasenko <thetarasenko.t@gmail.com>
2021-01-18 13:40:07 +0200

    Initial Commit

---
+++ Vertagelab_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vertagelab_test
+//
+//  Created by md760 on 18.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
